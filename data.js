e92b46115e1522b8ad6af6c3077802f89fa4bc47
ddewilde90 <ddewilde90@gmail.com>
2026-09-02 12:15:05 +0200

    Update image path in data.js

--- data.js
+++ data.js
@@ -30,7 +30,7 @@ export const data = {
             date: "2 SEPTEMBER 2026",
             intro: "Het idee dat je pas deugt als je in het plaatje past, wanneer een maatschappij besluit dat er maar één manier is om te focussen, één manier om te leren en er maar één route is om het goed te doen, maakt dat we ons ongemerkt superieur opstellen. Het dwingen van duizend unieke werelden in één starre mal is geen ordening; het is keiharde bekrompenheid. We vragen aan wie afwijkt om zichzelf te breken, enkel en alleen om in een bedacht hokje te passen.",
             content: "Volledige analyse volgt....",
-            image: "assets/images/tim.jpg",
+            image: "assets/images/tim uitsnede.jpg",
             tags: ["Economie", "Data"]
         },
         {
