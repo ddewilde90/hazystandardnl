3b6624655c059bfbb42910b886e455f7d0e89ebe
ddewilde90 <ddewilde90@gmail.com>
2026-09-24 17:05:31 +0200

    Correct disclaimer punctuation in data.js
    Fixed missing period at the end of a disclaimer.

--- data.js
+++ data.js
@@ -164,7 +164,7 @@ Praten over dwingende controle zou geen risico mogen zijn op institutionele afst
 
 <p class="text-sm italic text-gray-600">
 Ingrid Vledder & Ariane Hendriks, 'Met liefde heeft het niks te maken. Dwingende controle in intieme relaties', Uitgeverij SWP.<br>
-Evan Stark, 'Coercive Control: How Men Entrap Women in Personal Life', Oxford University Press. *Disclaimer: Dit is een algemeen, maatschappelijk artikel over de werking van dwingende controle, de patronen bij scheidingen en de knelpunten binnen het rechtssysteem en de hulpverlening. Het is gebaseerd op literatuur, ervaringen van diverse ouders en landelijke trends
+Evan Stark, 'Coercive Control: How Men Entrap Women in Personal Life', Oxford University Press. *Disclaimer: Dit is een algemeen, maatschappelijk artikel over de werking van dwingende controle, de patronen bij scheidingen en de knelpunten binnen het rechtssysteem en de hulpverlening. Het is gebaseerd op literatuur, ervaringen van diverse ouders en landelijke trends.
 </p>`,
             image: "https://unsplash.com/photos/man-holding-book-concrete-statue-SiCVttsKLMc",
             tags: ["Maatschappij", "Familierecht", "Dwingende Controle"]
